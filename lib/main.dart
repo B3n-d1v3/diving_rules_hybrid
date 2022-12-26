c8a97f72c48aecea09ec4a518f79055f65be4162
Benoit SUZANNE <ben.diving.judge@gmail.com>
2022-12-26 14:59:29 +0100

    test with Android studio git in dev

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Diving Rules Test 2',
+      title: 'Diving Rules Test 3',
       theme: ThemeData(
         // This is the theme of your application.
         //
