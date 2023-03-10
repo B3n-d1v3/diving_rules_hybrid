8ad84533209552d6651e98e134f8058315fefc95
Benoit SUZANNE <ben.diving.judge@gmail.com>
2023-03-10 16:03:44 +0100

    Updated the color palette tokens but did not sort out the cupertino and material color handling of dark mode

--- lib/tokens/colors.dart
+++ lib/tokens/colors.dart
@@ -1,22 +1,13 @@
 import 'package:flutter/widgets.dart';
 class AppColor {
   AppColor._();
-  static const Color dr_primary_light = Color(0xFF005A94);
-  static const Color dr_primary_dark = Color(0xFF009AFF);
-  static const Color dr_secondary_light = Color(0xFF009AFF);
-  static const Color dr_secondary_dark = Color(0xFF005994);
-  static const Color dr_positive_light = Color(0xFF006600);
-  static const Color dr_positive_dark = Color(0xFF00C300);
-  static const Color dr_negative_light = Color(0xFFAC0000);
-  static const Color dr_negative_dark = Color(0xFFFF614D);
-  static const Color dr_deselected_light = Color(0xFF575757);
-  static const Color dr_deselected_dark = Color(0xFF969696);
 
-  static const Color wa_mid_blue = Color(0xFF176CED);
-  static const Color wa_dark_blue = Color(0xFF0000BC);
-  static const Color wa_dark_grey = Color(0xFF5B6670);
-  static const Color wa_mid_grey = Color(0xFF8997A8);
-  static const Gradient wa_light_gradient = LinearGradient(
+  // World Aquatics colors
+  static const Color waColorMidBlue = Color(0xFF176CED);
+  static const Color waColorDarkBlue = Color(0xFF0000BC);
+  static const Color waColorDarkGrey = Color(0xFF5B6670);
+  static const Color waColorMidGrey = Color(0xFF8997A8);
+  static const Gradient waColorGradientLight = LinearGradient(
     begin: Alignment.centerLeft,
     end: Alignment.centerRight,
     colors: [
@@ -24,7 +15,7 @@ class AppColor {
       Color(0xff1756ED),
     ],
   );
-  static const Gradient wa_dark_gradient = LinearGradient(
+  static const Gradient waColorGradientDark = LinearGradient(
     begin: Alignment.centerLeft,
     end: Alignment.centerRight,
     colors: [
@@ -32,7 +23,7 @@ class AppColor {
       Color(0xff000080),
     ],
   );
-  static const Gradient wa_silver_gradient = LinearGradient(
+  static const Gradient waColorGradientSilver = LinearGradient(
     begin: Alignment.centerLeft,
     end: Alignment.centerRight,
     colors: [
@@ -40,8 +31,8 @@ class AppColor {
       Color(0xff5B6671),
     ],
   );
-  static const Color wa_diving_solid = Color(0xFF363A8B);
-  static const Gradient wa_diving_gradient = LinearGradient(
+  static const Color waColorDivingSolid = Color(0xFF363A8B);
+  static const Gradient waColorDivingGradient = LinearGradient(
     begin: Alignment.topLeft,
     end: Alignment.bottomRight,
     colors: [
@@ -49,4 +40,100 @@ class AppColor {
       Color(0xff3030A3),
     ],
   );
+
+  // App colors from iOS + Accessible
+  // static const Color dr_primary_light = Color(0xFF005A94);
+  // static const Color dr_primary_dark = Color(0xFF009AFF);
+  // static const Color dr_secondary_light = Color(0xFF009AFF);
+  // static const Color dr_secondary_dark = Color(0xFF005994);
+  // static const Color dr_positive_light = Color(0xFF006600);
+  // static const Color dr_positive_dark = Color(0xFF00C300);
+  // static const Color dr_negative_light = Color(0xFFAC0000);
+  // static const Color dr_negative_dark = Color(0xFFFF614D);
+  // static const Color dr_deselected_light = Color(0xFF575757);
+  // static const Color dr_deselected_dark = Color(0xFF969696);
+
+
+  // Non Material
+  static const Color drColorPositivelight = Color(0xFF006600);
+  static const Color drColorNegativeLight = Color(0xffAC0000);
+  static const Color drColorDeselectedlight = Color(0xFF575757);
+
+
+
+  // New App
+  // Light Mode
+  static const Color myBackgroundColorLight = Color(0xffffffff);
+  static const Color myOnBackgroundColorLight = Color(0xff000000);
+
+  static const Color myPrimaryColorLight = waColorDivingSolid;
+  static const Color myOnPrimaryColorLight = Color(0xFFFFFFFF);
+  static const Color mySecondaryColorLight = Color(0xFF005A94);
+  static const Color myOnSecondaryColorLight = Color(0xFFFFFFFF);
+  static const Color myTertiaryColorLight = Color(0xFF009AFF);
+  static const Color myOnTertiaryColorLight = Color(0xFFFFFFFF);
+
+  static const Color mySurfaceColorLight = Color(0xff000000);
+  static const Color myOnSurfaceColorLight = Color(0xffffffff);
+  static const Color myErrorColorLight = drColorNegativeLight;
+  static const Color myOnErrorColorLight = Color(0xffFFFFFF);
+
+  static const Color myPrimaryContainerColorLight = Color(0xFFe1e0ff);
+  static const Color myOnPrimaryContainerColorLight = Color(0xFF070764);
+  static const Color mySecondaryContainerColorLight = Color(0xFFD1E4FF);
+  static const Color myOnSecondaryContainerColorLight = Color(0xFF001D36);
+  static const Color myTertiaryContainerColorLight = Color(0xFFD1E4FF);
+  static const Color myOnTertiaryContainerColorLight = Color(0xFF001D36);
+  static const Color myErrorContainerColorLight = Color(0xFFFFDAD6);
+  static const Color myOnErrorContainerColorLight = Color(0xFF410002);
+
+  // static const Color myPrimaryVariantColorLight = Color(0xff7b1fa2); //
+  // static const Color mySecondaryVariantColorLight = Color(0xff26a69a); //
+  // const Color myBarBackgroundColorLight = Color(0xff6a1b9a); //
+  // const Color myScaffoldBackgroundColorLight = Color(0xff4a148c); //
+
+  // surfaceVariant: Color(0xFFDFE3EB),
+  // onSurfaceVariant: Color(0xFF42474E),
+  // outline: Color(0xFF73777F),
+  // onInverseSurface: Color(0xFFD6F6FF),
+  // inverseSurface: Color(0xFF00363F),
+  // inversePrimary: Color(0xFF9CCAFF),
+  // shadow: Color(0xFF000000),
+  // surfaceTint: Color(0xFF0062A0),
+  // outlineVariant: Color(0xFFC2C7CF),
+  // scrim: Color(0xFF000000),
+
+
+  // Dark Mode
+  static const Color dr_primary_dark = Color(0xFF009AFF);
+  static const Color dr_secondary_dark = Color(0xFF005994);
+
+  static const Color drColorPositiveDark = Color(0xFF00C300);
+  static const Color drColorNegativeDark = Color(0xFFFF614D);
+  static const Color drColorDeselectedDark = Color(0xFF969696);
+
+
+  static const Color myBackgroundColorDark = Color(0xff000000);
+  static const Color myOnBackgroundColorDark = Color(0xffffffff);
+
+  static const Color myPrimaryColorDark = Color(0xFF6B93EB);
+  static const Color myOnPrimaryColorDark = Color(0xFF000000);
+  static const Color mySecondaryColorDark = Color(0xFF009AFF);
+  static const Color myOnSecondaryColorDark = Color(0xFF000000);
+  static const Color myTertiaryColorDark = Color(0xFF005994);
+  static const Color myOnTertiaryColorDark = Color(0xFF000000);
+
+  static const Color mySurfaceColorDark = Color(0xffffffff);
+  static const Color myOnSurfaceColorDark = Color(0xff000000);
+  static const Color myErrorColorDark = drColorNegativeDark;
+  static const Color myOnErrorColorDark = Color(0xff000000);
+
+  static const Color myPrimaryContainerColorDark = Color(0xFF393d8f);
+  static const Color myOnPrimaryContainerColorDark = Color(0xFFe1e0ff);
+  static const Color mySecondaryContainerColorDark = Color(0xFF00497a);
+  static const Color myOnSecondaryContainerColorDark = Color(0xFFd0e4ff);
+  static const Color myTertiaryContainerColorDark = Color(0xFF00497a);
+  static const Color myOnTertiaryContainerColorDark = Color(0xFFd0e4ff);
+  static const Color myErrorContainerColorDark = Color(0xFF93000a);
+  static const Color myOnErrorContainerColorDark = Color(0xFFffdad6);
 }
--- lib/tokens/theme_data.dart
+++ lib/tokens/theme_data.dart
@@ -3,17 +3,87 @@ import 'package:flutter/cupertino.dart';
 
 import 'colors.dart';
 
-// Model used from https://www.youtube.com/watch?v=JkZnP1H0E6E
+// TODO: Later / Investigate Material you support
+// https://fivedottwelve.com/blog/themes-in-flutter-and-how-to-use-material-you-dynamic-colors/
 
+// Material Design Setup
+/*const lightColorScheme = ColorScheme(
+  brightness: Brightness.light,
+  primary: AppColor.myPrimaryColorLight,
+  onPrimary: AppColor.myOnPrimaryColorLight,
+  primaryContainer: AppColor.myPrimaryContainerColorLight,
+  onPrimaryContainer: AppColor.myOnPrimaryContainerColorLight,
+  secondary: AppColor.mySecondaryColorLight,
+  onSecondary: AppColor.myOnSecondaryColorLight,
+  secondaryContainer: AppColor.mySecondaryContainerColorLight,
+  onSecondaryContainer: AppColor.myOnSecondaryContainerColorLight,
+  tertiary: AppColor.myTertiaryColorLight,
+  onTertiary: AppColor.myOnTertiaryColorLight,
+  tertiaryContainer: AppColor.myTertiaryContainerColorLight,
+  onTertiaryContainer: AppColor.myOnTertiaryContainerColorLight,
+  error: AppColor.myErrorColorLight,
+  errorContainer: AppColor.myErrorContainerColorLight,
+  onError: AppColor.myOnErrorColorLight,
+  onErrorContainer: AppColor.myOnErrorContainerColorLight,
+  background: AppColor.myBackgroundColorLight,
+  onBackground: AppColor.myOnBackgroundColorLight,
+  surface: AppColor.mySurfaceColorLight,
+  onSurface: AppColor.myOnSurfaceColorLight,
+  // surfaceVariant: Color(0xFFDFE3EB),
+  // onSurfaceVariant: Color(0xFF42474E),
+  // outline: Color(0xFF73777F),
+  // onInverseSurface: Color(0xFFD6F6FF),
+  // inverseSurface: Color(0xFF00363F),
+  // inversePrimary: Color(0xFF9CCAFF),
+  // shadow: Color(0xFF000000),
+  // surfaceTint: Color(0xFF0062A0),
+  // outlineVariant: Color(0xFFC2C7CF),
+  // scrim: Color(0xFF000000),
+);*/
+
+/*const darkColorScheme = ColorScheme(
+  brightness: Brightness.dark,
+  primary: AppColor.myPrimaryColorDark,
+  onPrimary: AppColor.myOnPrimaryColorDark,
+  primaryContainer: AppColor.myPrimaryContainerColorDark,
+  onPrimaryContainer: AppColor.myOnPrimaryContainerColorDark,
+  secondary: AppColor.mySecondaryColorDark,
+  onSecondary: AppColor.myOnSecondaryColorDark,
+  secondaryContainer: AppColor.mySecondaryContainerColorDark,
+  onSecondaryContainer: AppColor.myOnSecondaryContainerColorDark,
+  tertiary: AppColor.myTertiaryColorDark,
+  onTertiary: AppColor.myOnTertiaryColorDark,
+  tertiaryContainer: AppColor.myTertiaryContainerColorDark,
+  onTertiaryContainer: AppColor.myOnTertiaryContainerColorDark,
+  error: AppColor.myErrorColorDark,
+  errorContainer: AppColor.myErrorContainerColorDark,
+  onError: AppColor.myOnErrorColorDark,
+  onErrorContainer: AppColor.myOnErrorContainerColorDark,
+  background: AppColor.myBackgroundColorDark,
+  onBackground: AppColor.myOnBackgroundColorDark,
+  surface: AppColor.mySurfaceColorDark,
+  onSurface: AppColor.myOnSurfaceColorDark,
+  // surfaceVariant: Color(0xFF42474E),
+  // onSurfaceVariant: Color(0xFFC2C7CF),
+  // outline: Color(0xFF8C9199),
+  // onInverseSurface: Color(0xFF001F25),
+  // inverseSurface: Color(0xFFA6EEFF),
+  // inversePrimary: Color(0xFF0062A0),
+  // shadow: Color(0xFF000000),
+  // surfaceTint: Color(0xFF9CCAFF),
+  // outlineVariant: Color(0xFF42474E),
+  // scrim: Color(0xFF000000),
+);*/
+
+// Cupertino theme setup
+// Model used from https://www.youtube.com/watch?v=JkZnP1H0E6E
 class Styles {
   static CupertinoThemeData themeData(bool isDarkTheme) {
     return CupertinoThemeData(
-          brightness: isDarkTheme
-              ? Brightness.dark
-              : Brightness.light,
-          primaryColor: isDarkTheme
-              ? AppColor.dr_primary_dark
-              : AppColor.dr_primary_light,
+      brightness: isDarkTheme ? Brightness.dark : Brightness.light,
+      primaryColor: isDarkTheme ? AppColor.myPrimaryColorDark : AppColor.myPrimaryColorLight,
+      // textTheme: isDarkTheme ? AppColor.myOnBackgroundColorDark : AppColor.myOnBackgroundColorLight,
+
           //textTheme: const TextTheme(
             //displayLarge: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
             //displayMedium:,
@@ -36,3 +106,5 @@ class Styles {
 
   }
 }
+
+
