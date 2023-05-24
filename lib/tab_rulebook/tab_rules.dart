a1230ef87210f2184d1f50b88eca06a7c13b9115
Benoit SUZANNE <ben.diving.judge@gmail.com>
2023-05-24 09:22:33 +0200

    Updated the EN & FR version of the Rules pdf documents

--- lib/tab_rulebook/tab_rules.dart
+++ lib/tab_rulebook/tab_rules.dart
@@ -1,15 +1,14 @@
-import 'package:flutter/cupertino.dart';
-import 'package:provider/provider.dart';
 import 'package:diving_rules_hybrid/provider/dark_theme_provider.dart';
-import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter_gen/gen_l10n/diving_rules_localizations.dart';
+import 'package:provider/provider.dart';
+import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
 
 class CupertinoTabRules extends StatefulWidget {
   const CupertinoTabRules({Key? key}) : super(key: key);
 
   @override
-  _CupertinoTabRulesState createState() =>
-      _CupertinoTabRulesState();
+  _CupertinoTabRulesState createState() => _CupertinoTabRulesState();
 }
 
 class _CupertinoTabRulesState extends State<CupertinoTabRules> {
@@ -17,29 +16,29 @@ class _CupertinoTabRulesState extends State<CupertinoTabRules> {
   Widget build(BuildContext context) {
     final themeState = Provider.of<DarkThemeProvider>(context);
     return CupertinoPageScaffold(
-      navigationBar: CupertinoNavigationBar(
-        middle: Text(AppLocalizations.of(context)!.cFBundleName),
+        navigationBar: CupertinoNavigationBar(
+          middle: Text(AppLocalizations.of(context)!.cFBundleName),
 
-        // Header trailing functions: Light/Dark switch + Language Selection
-        trailing: CupertinoButton(
-          child: Icon(
-              themeState.darkTheme ? CupertinoIcons.moon_stars_fill : CupertinoIcons.brightness_solid
+          // Header trailing functions: Light/Dark switch + Language Selection
+          trailing: CupertinoButton(
+            child: Icon(themeState.darkTheme
+                ? CupertinoIcons.moon_stars_fill
+                : CupertinoIcons.brightness_solid),
+            onPressed: () {
+              setState(() {
+                themeState.darkTheme = !themeState.darkTheme;
+                // themeModeSwitch = !themeModeSwitch;
+              });
+            },
+            padding: EdgeInsets.zero,
           ),
-          onPressed: () {
-            setState(() {
-              themeState.darkTheme = !themeState.darkTheme;
-              // themeModeSwitch = !themeModeSwitch;
-            });
-          },
-          padding: EdgeInsets.zero,
         ),
-      ),
-      child: Center(
-        // TODO: Select the Rule Book of the right localization
-        // Locale myLocale = Localizations.localeOf(context);
-         child: SfPdfViewer.asset('assets/rulebooks/2022-2025_FINA_EN_Diving-Technical-Rules.pdf')
-          //child: SfPdfViewer.asset('assets/rulebooks/2022-2025_Reglement-WA-Plongeon-FR_v1.pdf')
-        )
-      );
+        child: Center(
+            // TODO: Select the Rule Book of the right localization
+            // Locale myLocale = Localizations.localeOf(context);
+            child: SfPdfViewer.asset(
+                'assets/rulebooks/2022-2025_World-Aquatics-Diving-Rules_en.pdf')
+            //child: SfPdfViewer.asset('assets/rulebooks/2022-2025_Reglement-WA-Plongeon-v2_fr.pdf')
+            ));
   }
 }
