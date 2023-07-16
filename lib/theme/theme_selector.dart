6434561bab1501c3b533729a04a20c296d44dc4e
Benoit SUZANNE <ben.diving.judge@gmail.com>
2023-07-16 23:53:29 +0200

    updated the light / dark mode switcher to be a toogle icon mechanism

--- lib/theme/theme_selector.dart
+++ lib/theme/theme_selector.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -13,39 +14,43 @@ class ThemeSelector extends StatefulWidget {
 class _ThemeSelectorState extends State<ThemeSelector> {
   String themeValue = ThemeMode.system.toString();
 
+  final Map<String, IconData> themeIcons = {
+    ThemeMode.light.toString(): CupertinoIcons.sun_max_fill, // Icons.wb_sunny,
+    ThemeMode.dark.toString():
+        CupertinoIcons.moon_stars_fill, // Icons.nightlight_round,
+    ThemeMode.system.toString():
+        CupertinoIcons.square_line_vertical_square_fill, // Icons.circle_sharp,
+  };
+
   @override
   Widget build(BuildContext context) {
-    List<String> themeList = <String>[
-      ThemeMode.light.toString(),
-      ThemeMode.dark.toString(),
-      ThemeMode.system.toString()
-    ];
     var themeNotifier = Provider.of<ModelTheme>(context);
+    // correct the mode to set auto by default and to include the auto in the switch
 
-    return DropdownButton<String>(
-      value: themeValue,
-      icon: const Icon(Icons.arrow_downward),
-      elevation: 16,
-      onChanged: (String? value) {
-        setState(() {
-          themeValue = value!;
-        });
-        themeNotifier.themeMode = value == ThemeMode.system.toString()
-            ? ThemeMode.system
-            : (value == ThemeMode.dark.toString()
-                ? ThemeMode.dark
-                : ThemeMode.light);
-      },
-      items: themeList.map<DropdownMenuItem<String>>((String value) {
-        return DropdownMenuItem<String>(
-          value: value,
-          child: Icon(value == ThemeMode.system.toString()
-              ? Icons.circle_sharp
-              : (value == ThemeMode.dark.toString()
-                  ? Icons.nightlight_round
-                  : Icons.wb_sunny)),
-        );
-      }).toList(),
-    );
+    return
+        // Semantics(
+        // button: true,
+        // label: themeNotifier.themeMode == ThemeMode.light
+        //     ? AppLocalizations.of(context)!.themeSelectorSwitchDarkMode
+        //     : AppLocalizations.of(context)!.themeSelectorSwitchLightMode,
+        // child:
+        IconButton(
+            icon: Icon(themeNotifier.themeMode == ThemeMode.light
+                    ? CupertinoIcons.sun_max_fill //Icons.light_mode
+                    : CupertinoIcons.moon_stars_fill //Icons.dark_mode
+                ),
+            onPressed: () {
+              setState(() {
+                themeNotifier.themeMode =
+                    themeNotifier.themeMode == ThemeMode.light
+                        ? ThemeMode.dark
+                        : ThemeMode.light;
+              });
+            },
+            tooltip: themeNotifier.themeMode == ThemeMode.light
+                ? "Dark" // AppLocalizations.of(context)!.themeSelectorSwitchDarkMode
+                : "Light" // AppLocalizations.of(context)!.themeSelectorSwitchLightMode,
+            //    ), // Semantics
+            );
   }
 }
