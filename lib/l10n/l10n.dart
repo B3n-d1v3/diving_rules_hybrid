b2e9408c06a807a18880e7819f86394af63754dd
Benoit SUZANNE <ben.diving.judge@gmail.com>
2023-07-17 19:43:24 +0200

    Updates on the IT & ES to support the new 5 terms that have been added.

--- lib/l10n/l10n.dart
+++ lib/l10n/l10n.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+// regenerate translation files - in terminal: "flutter gen-l10n"
+
 class L10n {
   static final all = [
     const Locale('en'),
