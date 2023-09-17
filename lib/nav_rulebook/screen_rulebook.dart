443cc2257ac849b0f41b833c6020bc80412ee1a6
Benoit SUZANNE <ben.diving.judge@gmail.com>
2023-09-17 17:47:42 +0200

    updated the rulebook selection logic to set only the url selection in a dedicated class

--- lib/nav_rulebook/screen_rulebook.dart
+++ lib/nav_rulebook/screen_rulebook.dart
@@ -12,25 +12,36 @@ class _ScreenRulebookState extends State<ScreenRulebook> {
   @override
   Widget build(BuildContext context) {
     Locale myLocale = Localizations.localeOf(context);
+    // String rulebookUrl = 'assets/rulebooks/2022-2025_World-Aquatics-Diving-Rules_en.pdf';
     // debugPrint('>>>>> ScreenRulebook > myLocale.languageCode: ${myLocale.languageCode} - myLocale.countryCode: ${myLocale.countryCode}');
+
+    getRulebookUrl(Locale appLocale) {
+      String tempUrl =
+          'assets/rulebooks/2022-2025_World-Aquatics-Diving-Rules_en.pdf';
+      appLocale.languageCode == 'fr'
+          ? tempUrl =
+              'assets/rulebooks/2022-2025_Reglement-WA-Plongeon-v2_fr.pdf'
+          : appLocale.languageCode == 'es'
+              ? appLocale.countryCode == 'MX'
+                  ? tempUrl =
+                      'assets/rulebooks/2022-2025_Reglas-WA-Clavados-FMN_es_MX.pdf'
+                  : tempUrl =
+                      'assets/rulebooks/2022-2025_WA_Reglamento_Saltos_es.pdf'
+              // Add Italian rulebook here
+              // : appLocale.languageCode == 'it'
+              //     ? tempUrl = 'assets/rulebooks/2022-2025_xxxxxxx_it.pdf')
+              : tempUrl =
+                  'assets/rulebooks/2022-2025_World-Aquatics-Diving-Rules_en.pdf';
+      debugPrint(
+          '>>>>> ScreenRulebook > getRulebookUrl > tempUrl: ${tempUrl} ');
+      return tempUrl;
+    }
 
     return Scaffold(
         body: Center(
-            child: myLocale.languageCode == 'fr'
-                ? SfPdfViewer.asset(
-                    'assets/rulebooks/2022-2025_Reglement-WA-Plongeon-v2_fr.pdf')
-                // : myLocale.countryCode == 'es_MX'
-                : myLocale.languageCode == 'es'
-                    ? myLocale.countryCode == 'MX'
-                        ? SfPdfViewer.asset(
-                            'assets/rulebooks/2022-2025_Reglas-WA-Clavados-FMN_es_MX.pdf')
-                        : SfPdfViewer.asset(
-                            'assets/rulebooks/2022-2025_WA_Reglamento_Saltos_es.pdf')
-                    // Add Italian rulebook here
-                    // : myLocale.languageCode == 'it'
-                    //     ? SfPdfViewer.asset(
-                    //         'assets/rulebooks/2022-2025_xxxxxxx_it.pdf')
-                    : SfPdfViewer.asset(
-                        'assets/rulebooks/2022-2025_World-Aquatics-Diving-Rules_en.pdf')));
+            child: SfPdfViewer.asset(
+      getRulebookUrl(myLocale),
+      enableTextSelection: true,
+    )));
   }
 }
