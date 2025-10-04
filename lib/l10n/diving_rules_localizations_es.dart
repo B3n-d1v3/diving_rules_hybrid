aaebf197e90bd86602471a591f606636e62a0dba
Benoit SUZANNE <benoit.suzanne@orange.com>
2025-10-04 09:48:01 +0200

    Update of the spanish language es-es

--- lib/l10n/diving_rules_localizations_es.dart
+++ lib/l10n/diving_rules_localizations_es.dart
@@ -33,7 +33,7 @@ class AppLocalizationsEs extends AppLocalizations {
   String get penaltyDescription => 'Descripción';
 
   @override
-  String get penaltyPenalty => 'Penalidad';
+  String get penaltyPenalty => 'Penalizaciones';
 
   @override
   String get penaltyOwnership => 'Quien ?';
@@ -57,14 +57,14 @@ class AppLocalizationsEs extends AppLocalizations {
   String get buttonJudgeOpinion => 'Según opinión';
 
   @override
-  String get buttonReferee => 'Árbitro';
+  String get buttonReferee => 'Juez Árbitro';
 
   @override
   String get quizzIntroTitle => 'Practique sus conocimientos';
 
   @override
   String get quizzIntroDescription =>
-      'Para practicar en las Reglas de Saltos de World Aquatics, con este test podrás repasar el Resumen de Penalizaciones del Libro de Reglas a través de preguntas sobre las distintas reglas. \nEste test por sí solo no es suficiente para aprobar el examen de juez, ya que se centra en el resumen de penalizaciones, pero ayuda a aprender la asignación de penalizaciones de las reglas. \n\n Seleccione la cantidad de preguntas que deseas en tu cuestionario (de 5 a 40).';
+      'Para practicar las Reglas de Saltos de World Aquatics, con este test podrás repasar el Resumen de Penalizaciones del Reglamento a través de preguntas sobre las distintas reglas. \nEste test por sí solo no es suficiente para aprobar el examen de juez, ya que se centra en el resumen de penalizaciones, pero ayuda a aprender la asignación de penalizaciones de las reglas. \n\n Selecciona la cantidad de preguntas que deseas en tu cuestionario (de 5 a 40).';
 
   @override
   String get quizzIntroStartButton => 'Comenzar';
@@ -73,10 +73,10 @@ class AppLocalizationsEs extends AppLocalizations {
   String get quizzNext => 'Siguiente';
 
   @override
-  String get cFBundleDisplayName => 'diving rules 22-25';
+  String get cFBundleDisplayName => 'Reglas de saltos 22-25';
 
   @override
-  String get cFBundleName => 'diving rules';
+  String get cFBundleName => 'Reglas de saltos';
 
   @override
   String get quizzTitle => 'Cuestionario';
@@ -92,13 +92,13 @@ class AppLocalizationsEs extends AppLocalizations {
   String get quizzResultStartButton => 'Nuevo';
 
   @override
-  String get passed => 'Exito';
+  String get passed => 'Aprobado';
 
   @override
   String get failed => 'Fallado';
 
   @override
-  String get searchTitle => 'Buscar la regla';
+  String get searchTitle => 'Buscar la norma';
 
   @override
   String get ruleBookTitle => 'Reglamento';
@@ -120,11 +120,11 @@ class AppLocalizationsEs extends AppLocalizations {
 
   @override
   String get penalty0 =>
-      'Si el clavadista tarda más de un minuto, después de una advertencia.';
+      'Si el saltador tarda más de un minuto, después de un aviso.';
 
   @override
   String get penalty1 =>
-      'Si un clavadista realiza un doble rebote en el extremo del trampolín o un doble salto en el extremo de la plataforma antes del despegue.';
+      'Si un saltador realiza un doble rebote en el extremo del trampolín o un doble salto en el extremo de la plataforma antes del despegue.';
 
   @override
   String get penalty2 => 'Si el paso final no es con un pie.';
@@ -135,187 +135,186 @@ class AppLocalizationsEs extends AppLocalizations {
 
   @override
   String get penalty4 =>
-      'Si un giro es mayor o menor que el anunciado de 90 grados o más.';
+      'Si un giro es mayor o menor que el anunciado en \n90 grados o más.';
 
   @override
   String get penalty5 =>
-      'Si un clavadista ha realizado un clavado de un número distinto al anunciado.';
+      'Si un saltador ha realizado un salto de un número distinto al anunciado.';
 
   @override
   String get penalty6 =>
-      'Si los pies entran en el agua antes que la cabeza o las manos en una entrada con la cabeza por delante.';
+      'Si los pies entran en el agua antes que la cabeza o las manos en una entrada de cabeza.';
 
   @override
   String get penalty7 =>
-      'Si la cabeza o las manos entran en el agua antes que los pies en una entrada con los pies por delante.';
+      'Si la cabeza o las manos entran en el agua antes que los pies en una entrada de pie.';
 
   @override
   String get penalty8 =>
-      'Si se ha prestado ayuda al clavadista después de la señal de salida.';
+      'Si el saltador ha recibido ayuda después de la señal de inicio.';
 
   @override
-  String get penalty9 =>
-      'Cuando un segundo intento (un reinicio) no tiene éxito.';
+  String get penalty9 => 'Si falla un segundo intento (o reinicio).';
 
   @override
-  String get penalty10 =>
-      'Si un clavadista rechaza la ejecución de un Calvados.';
+  String get penalty10 => 'Si un saltador rehúsa la ejecución de un salto.';
 
   @override
   String get penalty11 =>
-      'En clavado sincronizado, si un clavadista o ambos clavadistas realizan un clavado de un número diferente.';
+      'En saltos sincronizados, si uno o los dos saltadores realizan un salto de diferente número.';
 
   @override
   String get penalty12 =>
-      'En los clavados sincronizados, si todos los jueces de ejecución de un clavadista otorgan cero (0) puntos.';
+      'En saltos sincronizados, si todos los jueces de ejecución otorgan cero (0) puntos a un saltador.';
 
   @override
   String get penalty13 =>
-      'En clavado sincronizado si todos los jueces de sincronización otorgan cero (0) puntos.';
+      'En saltos sincronizados, si todos los jueces de sincronización otorgan cero (0) puntos.';
 
   @override
   String get penalty14 =>
-      'En clavado sincronizado, si un clavadista o ambos clavadistas realizan un clavado de un  posición diferente.';
+      'En saltos sincronizados, si uno o los dos saltadores realizan un salto de diferente posición.';
 
   @override
   String get penalty15 =>
-      'En clavado sincronizado, si uno de los clavadistas entra en la superficie del agua antes de que el otro abandone el trampolín o la plataforma.';
+      'En saltos sincronizados, si uno de los dos saltadores alcanza la superficie del agua antes de que el otro saltador haya abandonado el trampolín o la plataforma.';
 
   @override
   String get penalty16 =>
-      'Si un clavadista da un paso y se detiene en la caminata o detiene el movimiento para un despegue de pie después de que las piernas hayan comenzado a presionar.';
+      'Cuando un saltador, da un paso y para en un salto con carrera o, en un salto sin carrera detiene el movimiento del despegue después de que las piernas hayan empezado a presionar la tabla.';
 
   @override
   String get penalty17 =>
-      'Si hay un nuevo intento en un clavado de pie, corriendo o un clavado en équilibrio.';
+      'Si un saltador hace un reinicio en un salto con carrera, en un salto sin carrera, o en un equilibrio.';
 
   @override
   String get penalty18 =>
-      'En clavado sincronizado, si hay un nuevo intento  por parte de uno o ambos clavadistas.';
+      'En saltos sincronizados si hay un reinicio por parte de uno o ambos saltadores.';
 
   @override
   String get penalty19 =>
-      'Si un clavadista realiza un clavado en una posición distinta a la anunciada.';
+      'Si un saltador ejecuta el salto en posición distinta a la anunciada.';
 
   @override
   String get penalty20 =>
-      'Si en un clavado, un clavadista esta muy cerca del trampolín o a la plataforma o toca el extremo del trampolín o de la plataforma con la cabeza.';
+      'Si durante la ejecución de un salto, el saltador está peligrosamente cerca del trampolín o plataforma o toca el final del trampolín o plataforma con la cabeza.';
 
   @override
   String get penalty21 =>
-      'Si un clavadista tiene uno o ambos brazos por encima de la cabeza en una entrada con los pies  o por debajo de la cabeza en una entrada con la cabeza.';
+      'Si uno o los dos brazos están situados por encima de la cabeza en una entrada de pie o por debajo de la cabeza en una entrada de cabeza.';
 
   @override
   String get penalty22 =>
-      'Si en un clavado al vuelo no se muestra claramente una posición extendida durante al menos un cuarto de salto mortal (90º) en los clavados con salto mortal.';
+      'Si en un salto al vuelo, la posición estirada no se muestra claramente al menos en un cuarto de mortal (90º) en saltos con un mortal.';
 
   @override
   String get penalty23 =>
-      'Si en un clavado al vuelo no se muestra claramente una posición extendida durante al menos medio salto mortal (180º) en clavados con más de 1 salto mortal.';
+      'Si en un salto al vuelo, la posición estirada no se muestra claramente al menos en medio mortal (180°) en saltos con más de un mortal.';
 
   @override
   String get penalty24 =>
-      'Si un clavado no se realiza en una posición como la descrita en el reglamento.';
+      'Si un salto no se realiza en una posición como la descrita en el reglamento.';
 
   @override
   String get penalty25 =>
-      'Si la posición de partida no es recta, cabeza erguida, con los brazos rectos en cualquier posición.';
+      ' Cuando en la posición de inicio no esté derecho con la cabeza erguida y los brazos extendidos, en cualquier posición';
 
   @override
   String get penalty26 =>
-      'Si los pies abandonan el trampolín o la plataforma (crow-hop) antes del despegue en una entrada de pie.';
+      'Si los pies se separan ligeramente del trampolín o plataforma antes del despegue';
 
   @override
   String get penalty27 =>
-      'Si en un clavado con equilibrio, no se muestra un equilibrio estacionario y firme en la posición vertical recta.';
+      'Cuando en un salto en equilibrio, no se muestre un equilibrio inmóvil y estable en la posición estirada vertical';
 
   @override
   String get penalty28 =>
-      'Si la caminata no es suave, estéticamente agradable en una dirección hacia adelante hasta el final del trampolín o plataforma.';
+      'Cuando la carrera no es fluida y estéticamente agradable, en dirección hacia delante hasta el final del trampolín o plataforma';
 
   @override
-  String get penalty29 => 'Si el despegue no es audaz, alto y seguro.';
+  String get penalty29 =>
+      'Cuando la salida no sea con equilibrio y altura, o en el extremo del trampolín o plataforma';
 
   @override
   String get penalty30 =>
-      'Si se trata de un clavado con giro, el giro se realiza manifiestamente desde el trampolín o la plataforma.';
+      'En saltos con tirabuzón, el tirabuzón no debe ejecutarse manifiestamente desde el trampolín o plataforma.';
 
   @override
   String get penalty31 =>
-      'Si la posición extendida no se muestra como se describen el reglamento.';
+      'Si la posición estirada no fuera estéticamente agradable, ni mostrada como se describe';
 
   @override
   String get penalty32 =>
-      'Si la posición en escuadra (B) no se muestra como se describe en el reglamento.';
+      'Si la posición carpada no fuera estéticamente agradable, ni mostrada como se describe';
 
   @override
   String get penalty33 =>
-      'Si en un clavado en escuadra con giro, la posición de escuadra no se muestra claramente.';
+      'En saltos carpados con tirabuzón, la posición carpada debe marcarse claramente';
 
   @override
   String get penalty34 =>
-      'Si la posición de bola (C) no se muestra como se describen el reglamento.';
+      'Si la posición encogida no fuera estéticamente agradable, ni mostrada como se describe';
 
   @override
   String get penalty35 =>
-      'Si se trata de un clavado con giro, la posición escuadra no se muestra claramente.';
+      'En saltos en posición encogidas con tirabuzón, la posición encogida debe marcarse claramente';
 
   @override
   String get penalty36 =>
-      'Si la posición libre no se muestra como se describe en el reglamento.';
+      'Si la posición discrecional no fuera mostrada como se describe';
 
   @override
   String get penalty37 =>
-      'Si los brazos no están en la posición correcta tanto en la entrada de cabeza como de pies.';
+      'cuando los brazos no estén en la posición correcta tanto en las entradas de cabeza como en las de pie';
 
   @override
   String get penalty38 =>
-      'En clavado sincronizado si no se muestra la similitud de la posición inicial, la aproximación, el despegue y la altura.';
+      'En saltos sincronizados, por defectos en: la similitud en la posición de inicio, carrera, salida o altura';
 
   @override
   String get penalty39 =>
-      'En clavado sincronizado si no se muestra la sincronización del movimiento durante el vuelo.';
+      'En saltos sincronizados si no se muestra coordinación en el tiempo de los movimientos durante el vuelo.';
 
   @override
   String get penalty40 =>
-      'En el clavado sincronizado si no se muestra la similitud de los ángulos verticales de las entradas.';
+      'En saltos sincronizados si no se muestra la similitud de los ángulos de las entradas respecto a la vertical';
 
   @override
   String get penalty41 =>
-      'En el clavado sincronizado si no se muestra la distancia comparativa desde el trampolín o la plataforma de la entrada.';
+      'En saltos sincronizados si no se muestra la comparación de la distancia en la entrada desde el trampolín o plataforma';
 
   @override
   String get penalty42 =>
-      'En clavado sincronizado si no se muestra el tiempo coordinado de las entradas.';
+      'En saltos sincronizado si no se muestra la coordinación en el tiempo de las entradas.';
 
   @override
   String get penalty43 =>
-      'Si un clavado se realiza parcialmente en una posición distinta a la anunciada.';
+      'Si un salto se ejecuta parcialmente en una posición distinta a la anunciada';
 
   @override
   String get penalty44 =>
-      'Si en un clavado, el clavadista se sumerge hacia el lado de la línea directa de vuelo.';
+      'Si durante la ejecución de un salto, el saltador salta hacia un lado respecto a la línea central del vuelo';
 
   @override
   String get penalty45 =>
-      'Si en un clavado, un clavadista toca el extremo del trampolín o la plataforma con los pies o las manos.';
+      'Si durante la ejecución de un salto, el saltador toca el extremo del trampolín o plataforma con sus pies o manos';
 
   @override
-  String get penalty46 => 'Si la entrada en el agua no es vertical, o casi.';
+  String get penalty46 => 'Si la entrada al agua es corta o pasada';
 
   @override
-  String get penalty47 => 'Si se tuerce la entrada en el agua.';
+  String get penalty47 => 'Si la entrada al agua está girada';
 
   @override
   String get penalty48 =>
-      'Si en la entrada en el agua el cuerpo no está recto.';
+      'Si en la entrada al agua el cuerpo no está extendido';
 
   @override
   String get penalty49 =>
-      'Si en la entrada en el agua los pies no están juntos, y los dedos no puntiagudos.';
+      'Si en la entrada al agua los pies no estén juntos o los empeines sin estirar';
 
   @override
-  String get quizzIntroQuestionNumber => 'Number of Questions: ';
+  String get quizzIntroQuestionNumber => 'Número de preguntas:';
 
   @override
   String get aboutTitle => 'Sobre';
@@ -325,13 +324,13 @@ class AppLocalizationsEs extends AppLocalizations {
 
   @override
   String get aboutDescription =>
-      'Esta aplicación ha sido creada para que puedas aprender y practicar las reglas de buceo de las normas de la World Aquatics.\nEsta aplicación no es una aplicación oficial de la World Aquatics, pero utiliza las reglas oficiales de la World Aquatics o sus traducciones oficiales como herramienta de trabajo.\n\nCon esta aplicación puedes:\n✔︎ Recuperar el regelmento en su idioma.\n✔︎ Revisar el resumen de las sanciones.\n✔︎ Practica con un cuestionario de resumen de penas.\n✔︎ Elige revisar todo en inglés';
+      'Esta aplicación ha sido creada para que puedas aprender y practicar las reglas de World Aquatics de saltos de trampolín.\nEsta aplicación no es una aplicación oficial de World Aquatics, pero utiliza las reglas oficiales de World Aquatics o sus traducciones oficiales como herramienta de trabajo.\n\nCon esta aplicación puedes:\n✔︎ Leer el reglamento.\n✔︎ Revisar el resumen de las penalizaciones.\n✔︎ Practicar con un test de penalizaciones.';
 
   @override
-  String get aboutFeedbackLink => 'propuestas';
+  String get aboutFeedbackLink => 'Propuestas';
 
   @override
-  String get aboutRulesReference => 'Normas de referencia : 2022-2025';
+  String get aboutRulesReference => 'Norma de referencia : 2022-2025';
 
   @override
   String get aboutLicense =>
@@ -339,11 +338,11 @@ class AppLocalizationsEs extends AppLocalizations {
 
   @override
   String get aboutThanks =>
-      'Muchas gracias a :\n• Daniel AZORIN por su apoyo en la finalización de la solicitud\n• Piero ITALIANI por su ayuda en la traducción al italiano.\n• Macarena SALGADO PATON y Daniel AZORIN por la traducción al español\n• Thomas HAUSOTTER  por la traducción al alemán';
+      'Muchas gracias a :\n•  Daniel AZORIN por su apoyo en la finalización de la solicitud.\n•  Piero ITALIANI por su ayuda en la traducción al italiano.\n•  Macarena SALGADO PATON e Daniel AZORIN por la traducción al español.\n•  Thomas HAUSOTTER  por la traducción al alemán';
 
   @override
   String get aboutHelp =>
-      'No dude en ponerse en contacto con nosotros:\n• Si está dispuesto a ayudar con las nuevas actualizaciones de las traducciones.\n• Para preguntas, comentarios e ideas sobre nuevas funciones.';
+      'No dude en ponerse en contacto con nosotros:\n•  Si está dispuesto a ayudar con las nuevas actualizaciones de las traducciones.\n•  Para preguntas, comentarios e ideas sobre nuevas funciones.';
 
   @override
   String get aboutDescriptionTitle => 'Descarga de la aplicación';
@@ -352,7 +351,7 @@ class AppLocalizationsEs extends AppLocalizations {
   String get aboutWADivingLink => 'WA Diving (🇬🇧)';
 
   @override
-  String get aboutWARulesLink => 'Últimas normas (🇬🇧)';
+  String get aboutWARulesLink => 'Últimas normativas (🇬🇧)';
 
   @override
   String get aboutLicenseTitle => 'Licencia';
@@ -361,7 +360,7 @@ class AppLocalizationsEs extends AppLocalizations {
   String get aboutThanksTitle => 'Gracias';
 
   @override
-  String get aboutFeedbackTitle => 'Dígame más';
+  String get aboutFeedbackTitle => 'Cuéntame más';
 
   @override
   String get aboutShareTitle => 'Compartir';
@@ -383,7 +382,7 @@ class AppLocalizationsEs extends AppLocalizations {
 
   @override
   String get quizzQuestionWarningNext =>
-      'Seleccione Sanción y por quién antes de pasar a la siguiente pregunta.';
+      'Seleccione Sanción y por quién se aplica antes de pasar a la siguiente pregunta.';
 
   @override
   String get rulebookCopy => 'Copia';
@@ -410,7 +409,7 @@ class AppLocalizationsEs extends AppLocalizations {
   String get rulebookSearchCancel => 'Cancelar búsqueda';
 
   @override
-  String get languageSelection => 'lengua';
+  String get languageSelection => 'Idioma';
 
   @override
   String get previous => 'Anterior';
